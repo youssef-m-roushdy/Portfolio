39192bb7d0d55a70be5b1d9924eb41a2452134fc
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-10-16 10:46:49 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -14,6 +15,20 @@ app.use(cors());
 app.use(express.json());
 
 //static files
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'images',
+        allowedFormats: ['jpeg', 'png', 'jpg'],
+    }
+});
 
 // Routes
 app.use('/api/v1/portfolio', require('./routes/portfolioRoute'))
