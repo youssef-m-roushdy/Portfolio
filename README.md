69e845d646f7e572a7b95ba8b4e2b9db9ea66b39
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-10-16 10:17:35 +0300

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# Youssef Mahmoud Roushdy Personal Portfolio
--- backend/routes/portfolioRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {sendEmailController} = require('../controllers/portfolioController');
-
-const router = express.Router()
-
-router.post('/sendEmail', sendEmailController)
-
-module.exports = router
