2de4c97dde2fcc4b1eada7de0b743d99852c1afd
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-10-30 11:43:00 +0300

    show all information about hiring offer

--- frontend/src/pages/Hire/HireDetails/HireDetails.jsx
+++ frontend/src/pages/Hire/HireDetails/HireDetails.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const HireDetails = () => {
+  const [offerDetails, setOfferDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate()
+  const { hireOfferId } = useParams();
+
+  useEffect(() => {
+    const getOfferDetails = async () => {
+      try {
+        const token = localStorage.getItem("auth-token");
+        if (!token) {
+          navigate('/');
+        }
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/hires/${hireOfferId}`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        setOfferDetails(res.data.hire);
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching offer details. Please try again.");
+        setLoading(false);
+        console.error("Error fetching offer details:", error);
+      }
+    };
+    getOfferDetails();
+  }, [hireOfferId, navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <div>HireDetails</div>
-  )
+    <div className="h-screen bg-white overflow-hidden shadow rounded-lg border">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Offer Details
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          This is all the information about the offer.
+        </p>
+      </div>
+      {offerDetails && (
+        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Sender Name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.name || "N/A"}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Email Address</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.email || "N/A"}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Phone Number</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.number || "N/A"}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Company</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.company || "N/A"}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Position</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.position || "N/A"}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Offer Letter</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {offerDetails.offerMessage || "N/A"}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      )}
+      <Link to="/hire" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Back</Link>
+    </div>
+  );
 }
 
-export default HireDetails
+export default HireDetails;
